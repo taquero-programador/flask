11d2d50bc0ceac3238d8b06d0403f5482886262b
taquero-programador <jrangel93@protonmail.com>
2023-04-08 23:47:57 -0600

    feat(03): forms

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -18,3 +19,13 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data},\
+              remember_me={form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
